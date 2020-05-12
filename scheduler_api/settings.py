7e54d07af5780c7a4f9f02ac245e944032e4f7ba
mkd63 <sharmaaahan15@gmail.com>
2020-05-12 22:36:47 +0530

    created .env, added python-decouple

--- scheduler_api/settings.py
+++ scheduler_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,13 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^&_ze%*)@q^s_0xr#1!y46^9n!mcbc*opt683zsw)&r8n0v@%b'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [config("ALLOWED_HOST")]
 
 # Application definition
 
@@ -85,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'scheduler_development',
         'USER': 'postgres',
-        'PASSWORD': 'aahaNoob63',
+        'PASSWORD': config('DATABASE_URL'),
         'HOST': 'localhost',
         'PORT': ''                 # set to empty string for default
     }
